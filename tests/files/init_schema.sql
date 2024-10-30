1c82e5afa44a7ca10f354f0f0305192c2d2b5692
Ryan Rymarczyk <rymarczy@gmail.com>
2024-10-30 16:57:44 -0400

    re-factor opmi box load

--- tests/files/init_schema.sql
+++ tests/files/init_schema.sql
@@ -968,6 +968,27 @@ CREATE TABLE surveys.dashboard_survey_questions_archive (
     archive_time timestamp with time zone
 );
 
+CREATE TABLE surveys.csat (
+        survey_date DATE,
+        survey_name VARCHAR(255),
+        question_description TEXT,
+        response_total INTEGER,
+        response_1_text VARCHAR(255),
+        response_1_percent FLOAT,
+        response_2_text VARCHAR(255),
+        response_2_percent FLOAT,
+        response_3_text VARCHAR(255),
+        response_3_percent FLOAT,
+        response_4_text VARCHAR(255),
+        response_4_percent FLOAT,
+        response_5_text VARCHAR(255),
+        response_5_percent FLOAT,
+        response_6_text VARCHAR(255),
+        response_6_percent FLOAT,
+        response_7_text VARCHAR(255),
+        response_7_percent FLOAT
+);
+
 -- ****************************************** --
 -- END INIT GITHUB ACTIONS SCHEMA --
 -- ****************************************** --
