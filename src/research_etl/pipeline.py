bb0600b46e0c0f3a337987daeeb33a68598b2759
Ryan Rymarczyk <rymarczy@gmail.com>
2025-04-01 11:33:51 -0400

    drop box job

--- src/research_etl/pipeline.py
+++ src/research_etl/pipeline.py
@@ -2,7 +2,6 @@ import os
 
 from research_etl.etl_korbato.korbato_job import run as odx_job
 from research_etl.etl_afc.afc_job import run as afc_job
-from research_etl.mbta_open_data.box_loader_job import run as box_import_job
 
 from research_etl.utils.util_aws import check_for_parallel_tasks
 from research_etl.utils.util_rds import DatabaseManager
@@ -20,7 +19,6 @@ def run_jobs() -> None:
 
     odx_job(db_manager)
     afc_job(db_manager)
-    box_import_job()
 
 
 if __name__ == "__main__":
