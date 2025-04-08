63c742242d05f53e343199189e75e72fca1fce85
Ryan Rymarczyk <rymarczy@gmail.com>
2025-04-08 05:52:21 -0400

    run job out/20250402

--- src/research_etl/pipeline.py
+++ src/research_etl/pipeline.py
@@ -1,7 +1,6 @@
 import os
 
-from research_etl.etl_korbato.korbato_job import run as odx_job
-from research_etl.etl_afc.afc_job import run as afc_job
+from research_etl.etl_korbato.korbato_job import alt_run as odx_job
 
 from research_etl.utils.util_aws import check_for_parallel_tasks
 from research_etl.utils.util_rds import DatabaseManager
@@ -18,7 +17,6 @@ def run_jobs() -> None:
     db_manager = DatabaseManager()
 
     odx_job(db_manager)
-    afc_job(db_manager)
 
 
 if __name__ == "__main__":
