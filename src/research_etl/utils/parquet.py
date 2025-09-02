77b1fcedc2688574729f1f3078ca742d49f253da
Ted Alexander <talexander3@mbta.com>
2025-09-02 18:08:20 -0400

    Formatting fixes

--- src/research_etl/utils/parquet.py
+++ src/research_etl/utils/parquet.py
@@ -12,7 +12,7 @@ import pyarrow.parquet as pq
 import pyarrow.dataset as pd
 import pyarrow.compute as pc
 import pyarrow.acero as ac
-import pyarrow.fs as fs
+from pyarrow import fs
 
 from research_etl.utils.util_logging import ProcessLogger
 from research_etl.utils.util_aws import list_objects
@@ -80,16 +80,18 @@ def ds_from_path(source: Union[str, Sequence[str]], session: boto3.Session = Non
         paths = [f for f in source if f.endswith(".parquet")]
     log.add_metadata(num_source=len(paths), paths=",".join(paths))
 
-    if source.startswith("s3://"): # TODO: temporary workaround for pyarrow s3 permissions issue
+    if isinstance(source, str) and source.startswith("s3://"):  # temporary workaround for pyarrow s3 permissions issue
         sts_client = boto3.client("sts")
-        assumed_role = sts_client.assume_role(RoleArn=os.getenv("ROLE_ARN_WITHIN_TID_FOR_KMS_ACCESS", ""), RoleSessionName='session_name')
-        credentials = assumed_role['Credentials']
+        assumed_role = sts_client.assume_role(
+            RoleArn=os.getenv("ROLE_ARN_WITHIN_TID_FOR_KMS_ACCESS", ""), RoleSessionName="session_name"
+        )
+        credentials = assumed_role["Credentials"]
 
         s3 = fs.S3FileSystem(
             access_key=credentials["AccessKeyId"],
             secret_key=credentials["SecretAccessKey"],
             session_token=credentials["SessionToken"],
-            region='us-east-1'
+            region="us-east-1",
         )
 
         # remove s3:// prefix (pyarrow doesn't accept it if filesystem is explicitly provided)
